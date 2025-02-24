54ed3b3831bfd159eb1941c6df22e46b33e906ad
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-02-24 18:57:32 +0800

    Update customize.sh
    Simplify certain processes to match the changes in aautilities.sh
    简化部分流程以匹配 aautilities.sh 的更改

--- module/customize.sh
+++ module/customize.sh
@@ -1,12 +1,10 @@
 #!/system/bin/sh
 SKIPUNZIP=1
-MODNAME=$(grep_prop name "${TMPDIR}/module.prop")
-CONFIGDIR=/data/adb/bloatwareslayer
-LOGDIR="$CONFIGDIR/logs"
+MOD_NAME=$(grep_prop name "${TMPDIR}/module.prop")
+CONFIG_DIR=/data/adb/bloatwareslayer
+LOG_DIR="$CONFIG_DIR/logs"
 
 ui_print "- Extract aautilities.sh"
-ui_print "- ZIPFILE: $ZIPFILE"
-ui_print "- TMPDIR: $TMPDIR"
 ui_print "- MODPATH: $MODPATH"
 unzip -o "$ZIPFILE" 'aautilities.sh' -d "$TMPDIR" >&2
 if [ ! -f "$TMPDIR/aautilities.sh" ]; then
@@ -17,30 +15,30 @@ fi
 . "$TMPDIR/aautilities.sh"
 
 module_install_proc(){
-  ui_print "- Configuring $MODNAME"
-  ui_print "- Extract module file(s)"
+  ui_print "- Setting up $MOD_NAME"
+  ui_print "- Extract module(s) file(s)"
+  extract "$ZIPFILE" 'aautilities.sh' "$MODPATH"
   extract "$ZIPFILE" 'module.prop'     "$MODPATH"
   extract "$ZIPFILE" 'post-fs-data.sh' "$MODPATH"
+  extract "$ZIPFILE" 'service.sh' "$MODPATH"
   extract "$ZIPFILE" 'uninstall.sh' "$MODPATH"
-  if [ ! -d "$LOGDIR" ]; then
-    ui_print "- $CONFIGDIR does not exist"
-    ui_print "- $LOGDIR does not exist"
-    mkdir -p "$LOGDIR" || abort "! Failed to create $LOGDIR!"
-    ui_print "- Create $CONFIGDIR"
-    ui_print "- Create $LOGDIR"
+  if [ ! -d "$LOG_DIR" ]; then
+    ui_print "- $LOG_DIR does not exist"
+    mkdir -p "$LOG_DIR" || abort "! Failed to create $LOG_DIR!"
+    ui_print "- Create $LOG_DIR"
   fi
-  if [ ! -f "$CONFIGDIR/target.txt" ]; then
+  if [ ! -f "$CONFIG_DIR/target.txt" ]; then
     ui_print "- target.txt does not exist"
     extract "$ZIPFILE" 'target.txt' "$TMPDIR"
-    mv "$TMPDIR/target.txt" "$CONFIGDIR/target.txt" || abort "! Failed to create target.txt!"
+    mv "$TMPDIR/target.txt" "$CONFIG_DIR/target.txt" || abort "! Failed to create target.txt!"
   else
     ui_print "- Detect existed target.txt"
-    ui_print "- Skip overwritting target.txt"
+    ui_print "- Skip overwriting target.txt"
   fi
 }
 
 show_system_info
-enforce_install_from_magisk_app
+enforce_install_from_magisk_app "$CONFIG_DIR"
 module_install_proc
 set_module_files_perm
-ui_print "- Welcome to use ${MODNAME}!"
+ui_print "- Welcome to use ${MOD_NAME}!"
