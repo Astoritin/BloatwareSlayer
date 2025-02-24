4963c580430fa9fe973c3a0ba29a205c5e5f1955
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-02-24 19:01:14 +0800

    Update post-fs-data.sh
    Support KernelSU officially
    正式支持 KernelSU
    Change the mount method for KernelSU and APatch from mknod to mount directly
    更改 KernelSU 和 APatch 的挂载方法，从 mknod 改为直接挂载

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -4,26 +4,33 @@ MODDIR=${0%/*}
 CONFIG_DIR=/data/adb/bloatwareslayer
 LOG_DIR="$CONFIG_DIR/logs"
 BRICKED_STATUS="$CONFIG_DIR/bricked"
-BS_LOG_FILE="$LOG_DIR/log_pfd_$(date +"%Y-%m-%d_%H-%M-%S").txt"
 TARGET_LIST="$CONFIG_DIR/target.txt"
+EMPTY_DIR="$CONFIG_DIR/empty"
+BS_LOG_FILE="$LOG_DIR/log_$(date +"%Y-%m-%d_%H-%M-%S")_pfd.txt"
 SYSTEM_APP_PATHS="/system/app /system/product/app /system/product/priv-app /system/priv-app /system/system_ext/app /system/system_ext/priv-app"
 MODULE_PROP="$MODDIR/module.prop"
+
+. "$MODDIR/aautilities.sh"
 
 if [ ! -d "$LOG_DIR" ]; then
     mkdir -p "$LOG_DIR"
 fi
+ROOT_IMP=$(cat "$CONFIG_DIR/root.txt")
+if [ ! -f "$CONFIG_DIR/root.txt" ]; then
+    enforce_install_from_magisk_app "$CONFIG_DIR"
+    ROOT_IMP=$(cat "$CONFIG_DIR/root.txt")
+fi
 
-echo " " >> $BS_LOG_FILE
 echo "Bloatware Slayer" >> $BS_LOG_FILE
 echo "By Astoritin Ambrosius" >> $BS_LOG_FILE
 echo "- Version: 1.0.3" >> $BS_LOG_FILE
-echo "- current time stamp: $(date +"%Y-%m-%d %H:%M:%S")" >> $BS_LOG_FILE
+echo "- Current time stamp: $(date +"%Y-%m-%d %H:%M:%S")" >> $BS_LOG_FILE
 echo "- Starting post-fs-data.sh..." >> $BS_LOG_FILE
 echo "- LOG_DIR: $LOG_DIR" >> $BS_LOG_FILE
 echo "- BS_LOG_FILE: $BS_LOG_FILE" >> $BS_LOG_FILE
 echo "- TARGET_LIST: $TARGET_LIST" >> $BS_LOG_FILE
 echo "- BRICKED_STATUS: $BRICKED_STATUS" >> $BS_LOG_FILE
-echo "- MODULE_PROP: $MODULE_PROR" >> $BS_LOG_FILE
+echo "- MODULE_PROP: $MODULE_PROP" >> $BS_LOG_FILE
 
 echo "- Removing old folders and .replace..." >> $BS_LOG_FILE
 if [ -d "$MODDIR/system" ]; then
@@ -35,35 +42,40 @@ fi
 
 if [ -f "$BRICKED_STATUS" ]; then
     echo "- Detect bricked status!" >> $BS_LOG_FILE
-    DESCRIPTION="[🚫Auto disabled from brick] 勝った、勝った、また勝ったぁっと！🎉✨"
+    DESCRIPTION="[❌Disabled. 🚫Auto disabled from brick! 💡Root:$ROOT_IMP] 勝った、勝った、また勝ったぁーっと！！🎉✨"
     sed -i "/^description=/c\description=$DESCRIPTION" "$MODULE_PROP" || {
         echo "! Failed to update module.prop!" >> $BS_LOG_FILE
-        echo "- Attempt to create disable Bloatware Slayer manually..." >> $BS_LOG_FILE
+        echo "- Attempt to create disable manually..." >> $BS_LOG_FILE
         touch "${MODDIR}/disable"
         if [ -f "${MODDIR}/disable" ]; then
-            echo "- Created file disable." >> $BS_LOG_FILE
+            echo '- Create file "disable"' >> $BS_LOG_FILE
         else
-            echo "! Failed to create disable file!" >> $BS_LOG_FILE
+            echo '! Failed to create file "disable"!' >> $BS_LOG_FILE
         fi
         exit 1
     }
-    echo "- Updated module.prop done." >> $BS_LOG_FILE
-    echo "- Skip post-fs-data.sh process..." >> $BS_LOG_FILE
+    echo "- Update module.prop" >> $BS_LOG_FILE
+    echo "- Skip post-fs-data.sh process" >> $BS_LOG_FILE
     exit 1
 else
-    echo "Bricked status file not exist, keep going..." >> $BS_LOG_FILE
+    echo "- Not bricked, keep going" >> $BS_LOG_FILE
 fi
 
 if [ ! -f "$TARGET_LIST" ]; then
-    echo "! Target list file ($TARGET_LIST) does not exist!" >> $BS_LOG_FILE
+    echo "! Target list does not exist!" >> $BS_LOG_FILE
     exit 1
 fi
+
+if [ ! -d "$EMPTY_DIR" ]; then
+    echo "- Create $EMPTY_DIR" >> $BS_LOG_FILE
+    mkdir -p "$EMPTY_DIR" >> $BS_LOG_FILE
+fi 
 
 TOTAL_APPS_COUNT=0
 BLOCKED_APPS_COUNT=0
 while IFS= read -r package; do
     package=$(echo "$package" | tr -d '\r')
- #   echo "- current line: $package" >> $BS_LOG_FILE
+    echo "- Current line: $package" >> $BS_LOG_FILE
     if [ -z "$package" ]; then
         echo "- Detect empty line, skip processing" >> $BS_LOG_FILE
         continue
@@ -71,54 +83,59 @@ while IFS= read -r package; do
         echo '- Detect comment symbol "#", skip processing' >> $BS_LOG_FILE
         continue
     fi
-    echo "- Processing package: $package" >> $BS_LOG_FILE
+    echo "- Process App: $package" >> $BS_LOG_FILE
     TOTAL_APPS_COUNT=$((TOTAL_APPS_COUNT+1))
     for path in $SYSTEM_APP_PATHS; do
         app_path="$path/$package"
         echo "- Checking dir: $app_path" >> $BS_LOG_FILE
         if [ -d "$app_path" ]; then
-            if [ "$KSU" ] || [ "$APATCH" ]; then
-                mknod "$MODDIR/$app_path" c 0 0
-                echo "- Set mknod for $app_path" >> $BS_LOG_FILE
+            echo "- Create $MODDIR/$app_path" >> $BS_LOG_FILE
+            mkdir -p "$MODDIR/$app_path" >> $BS_LOG_FILE
+            if [ -n "$KSU" ] || [ -n "$APATCH" ]; then
+                echo "- Execute mount -o bind $EMPTY_DIR $app_path" >> $BS_LOG_FILE
+                mount -o bind "$EMPTY_DIR" "$app_path"
+                if [ $? -eq 0 ]; then
+                    echo "- Succeeded." >> $BS_LOG_FILE
+                else
+                    echo "- Failed, error code: $?" >> $BS_LOG_FILE
+                fi
                 BLOCKED_APPS_COUNT=$((BLOCKED_APPS_COUNT + 1))
                 break
             else
-                mkdir -p "$MODDIR/$app_path"
                 touch "$MODDIR/$app_path/.replace"
-                echo "- Created .replace file for $app_path" >> $BS_LOG_FILE
+                echo "- Create .replace file for $app_path" >> $BS_LOG_FILE
                 BLOCKED_APPS_COUNT=$((BLOCKED_APPS_COUNT + 1))
                 break
             fi
         else
-            echo "- dir not found: $app_path" >> $BS_LOG_FILE
+            echo "- Dir not found: $app_path" >> $BS_LOG_FILE
         fi
     done
 done < "$TARGET_LIST"
 
 APP_NOT_FOUND=$((TOTAL_APPS_COUNT - BLOCKED_APPS_COUNT))
-echo "- $TOTAL_APPS_COUNT App(s) in total" >> $BS_LOG_FILE
-echo "- Processed $BLOCKED_APPS_COUNT App(s)" >> $BS_LOG_FILE
-echo "- $APP_NOT_FOUND App(s) are not found" >> $BS_LOG_FILE
+echo "- $TOTAL_APPS_COUNT APP(s) in total" >> $BS_LOG_FILE
+echo "- $BLOCKED_APPS_COUNT APP(s) slain" >> $BS_LOG_FILE
+echo "- $APP_NOT_FOUND APP(s) not found" >> $BS_LOG_FILE
 
 if [ -f "$MODULE_PROP" ]; then
     if [ $BLOCKED_APPS_COUNT -gt 0 ]; then
-        DESCRIPTION="[🎯$TOTAL_APPS_COUNT App(s) WANTED 😋$BLOCKED_APPS_COUNT App(s) SLAIN 👀$APP_NOT_FOUND App(s) MISSED] 勝った、勝った、また勝ったぁっと！🎉✨"
+        DESCRIPTION="[✅Enabled. 😋Blocked:$BLOCKED_APPS_COUNT APP(s), 👀Not found:$APP_NOT_FOUND APP(s), 📄Targeted:$TOTAL_APPS_COUNT APP(s), 💡Root:$ROOT_IMP] 勝った、勝った、また勝ったぁーっと！！🎉✨"
     else
         if [ $TOTAL_APPS_COUNT -gt 0]; then
-            DESCRIPTION="[🎯$TOTAL_APPS_COUNT App(s) WANTED 😴No targets today...] 勝った、勝った、また勝ったぁっと！🎉✨"
+            DESCRIPTION="[✅Enabled. ⭕No APP blocked yet, 📄Targeted:$TOTAL_APPS_COUNT APP(s), 💡Root:$ROOT_IMP] 勝った、勝った、また勝ったぁーっと！！🎉✨"
         else
-            DESCRIPTION="[❌Abnormal status] 勝った、勝った、また勝ったぁっと！🎉✨"
+            DESCRIPTION="[❌Disabled. 🤯Abnormal status! 💡Root:$ROOT_IMP] 勝った、勝った、また勝ったぁーっと！！🎉✨"
         fi
     fi
     sed -i "/^description=/c\description=$DESCRIPTION" "$MODULE_PROP" || {
-        echo "- Failed to update module.prop. Please check permissions." >> $BS_LOG_FILE
+        echo "- Failed to update module.prop" >> $BS_LOG_FILE
         exit 1
     }
-    echo "- Updated module.prop with blocked app count: $DESCRIPTION" >> $BS_LOG_FILE
+    echo "- Update module.prop:$DESCRIPTION" >> $BS_LOG_FILE
 else
-    echo "- module.prop not found. Skip updating." >> $BS_LOG_FILE
+    echo "- module.prop not found, skip updating" >> $BS_LOG_FILE
 fi
 
 echo "- post-fs-data.sh case closed!" >> $BS_LOG_FILE
 echo "- All done!" >> $BS_LOG_FILE
-echo " " >> $BS_LOG_FILE
