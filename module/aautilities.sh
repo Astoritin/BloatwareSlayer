f641da00b55b462c59c6f6e1b3317ce3c02abd2b
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-02-26 23:14:28 +0800

    Update aautilities.sh

--- module/aautilities.sh
+++ module/aautilities.sh
@@ -1,63 +1,90 @@
-#!/system/bin/sh
-MODDIR=${0%/*}
+VERIFY_DIR="$TMPDIR/.aa_bs_verify"
+mkdir "$VERIFY_DIR"
 
-enforce_install_from_magisk_app(){
-
+install_env_check() {
   local CONFIG_DIR="$1"
-  local MAGISK_OUI=Official
-  local ROOT_IMP=Unknown
-
-  if [ ! -d "$CONFIG_DIR" ]; then
-    ui_print "- $CONFIG_DIR does not exist"
-    mkdir -p "$CONFIG_DIR" || abort "! Failed to create $CONFIG_DIR!"
-    ui_print "- Create $CONFIG_DIR"
-  fi
-
-  if [ "$BOOTMODE" ] && [ "$KSU" ]; then
-  ui_print "- Install from KernelSU APP"
-  ui_print "- KernelSU version: $KSU_KERNEL_VER_CODE (kernel) + $KSU_VER_CODE (ksud)"
-  ROOT_IMP="KernelSU (kernel:$KSU_KERNEL_VER_CODE, ksud:$KSU_VER_CODE)"
-  if [ "$(which magisk)" ]; then
-    ui_print "! Detect multiple Root implements!"
-    ROOT_IMP="Multiple"
-  fi
-  elif [ "$BOOTMODE" ] && [ "$APATCH" ]; then
-  ui_print "- Install from APatch APP"
-  ui_print "- APatch version: $APATCH_VER_CODE"
-  ROOT_IMP="APatch ($APATCH_VER_CODE)"
-  elif [ "$BOOTMODE" ] && [ "$MAGISK_VER_CODE" ]; then
-    if [[ "$MAGISK_VER" == *"-alpha" ]]; then
-    MAGISK_OUI="Magisk Alpha"
-    ROOT_IMP="Magisk Alpha ($MAGISK_VER_CODE)"
-    elif [[ "$MAGISK_VER" == *"-lite" ]]; then
-    MAGISK_OUI="Magisk Lite"
-    ROOT_IMP="Magisk Lite ($MAGISK_VER_CODE)"
-    elif [[ "$MAGISK_VER" == *"-kitsune" ]]; then
-    MAGISK_OUI="Kitsune Mask"
-    ROOT_IMP="Kitsune Mask ($MAGISK_VER_CODE)"
-    elif [[ "$MAGISK_VER" == *"-delta" ]]; then
-    MAGISK_OUI="Magisk Delta"
-    ROOT_IMP="Magisk Delta ($MAGISK_VER_CODE)"
-    else
-    ROOT_IMP="Magisk ($MAGISK_VER_CODE)"
+  local MAGISK_BRANCH_NAME="Official"
+  local ROOT_IMP="Magisk"
+  if [[ "$KSU" ]]; then
+    echo "- Install from KernelSU"
+    echo "- KernelSU version: $KSU_KERNEL_VER_CODE (kernel) + $KSU_VER_CODE (ksud)"
+    ROOT_IMP="KernelSU (kernel:$KSU_KERNEL_VER_CODE, ksud:$KSU_VER_CODE)"
+    if [[ "$(which magisk)" ]]; then
+      echo "! Detect multiple Root implements!"
+      ROOT_IMP="Multiple"
     fi
-  ui_print "- Install from $MAGISK_OUI APP"
-  ui_print "- Magisk version: $MAGISK_VER ($MAGISK_VER_CODE)"
+  elif [[ "$APATCH" ]]; then
+    echo "- Install from APatch"
+    echo "- APatch version: $APATCH_VER_CODE"
+    ROOT_IMP="APatch ($APATCH_VER_CODE)"
+  elif [[ "$MAGISK_VER_CODE" || -n "$(magisk -v || magisk -V)" ]]; then
+    MAGISK_V_VER_NAME="$(magisk -v)"
+    MAGISK_V_VER_CODE="$(magisk -V)"
+    if [[ "$MAGISK_VER" == *"-alpha"* || "$MAGISK_V_VER_NAME" == *"-alpha"* ]]; then
+      MAGISK_BRANCH_NAME="Magisk Alpha"
+    elif [[ "$MAGISK_VER" == *"-lite"* || "$MAGISK_V_VER_NAME" == *"-lite"* ]]; then
+      MAGISK_BRANCH_NAME="Magisk Lite"
+    elif [[ "$MAGISK_VER" == *"-kitsune"* || "$MAGISK_V_VER_NAME" == *"-kitsune"* ]]; then
+      MAGISK_BRANCH_NAME="Kitsune Mask"
+    elif [[ "$MAGISK_VER" == *"-delta"* || "$MAGISK_V_VER_NAME" == *"-delta"* ]]; then
+      MAGISK_BRANCH_NAME="Magisk Delta"
+    else
+      MAGISK_BRANCH_NAME="Magisk"
+    fi
+    ROOT_IMP="$MAGISK_BRANCH_NAME (${MAGISK_VER_CODE:-$MAGISK_V_VER_CODE})"
+    echo "- Install from $ROOT_IMP"
   else
-  ui_print "! Install modules in Recovery mode is not support!"
-  about "! Please install this module in Magisk / KernelSU / APatch APP!"
+    ROOT_IMP="Recovery"
+    echo "! Install module in Recovery mode is not support!"
+    echo "! Especially for KernelSU / APatch!"
+    abort "! Please install this module in Magisk / KernelSU / APatch APP!"
   fi
-  echo "$ROOT_IMP" > "$CONFIG_DIR/root.txt"
+  if [ -n "$CONFIG_DIR" ]; then
+    if [ ! -d "$CONFIG_DIR" ]; then
+        echo "- $CONFIG_DIR does not exist"
+        mkdir -p "$CONFIG_DIR" || abort "! Failed to create $CONFIG_DIR!"
+        echo "- Create $CONFIG_DIR"
+    else
+        echo "- $CONFIG_DIR already existed"
+    fi
+    if ! grep -q "^root=" "$CONFIG_DIR/status.info"; then
+    echo "root=" >> "$CONFIG_DIR/status.info"
+    fi
+    sed -i "/^root=/c\root=$ROOT_IMP" "$CONFIG_DIR/status.info"
+  fi
+}
+
+debug_print_values(){
+  OUTPUT_LOG="$1"
+  env >> $OUTPUT_LOG
+  echo "- BOOTMODE: $BOOTMODE" >> $OUTPUT_LOG
+  echo "- KSU: $KSU" >> $OUTPUT_LOG
+  echo "- KSU_KERNEL_VER_CODE: $KSU_KERNEL_VER_CODE" >> $OUTPUT_LOG
+  echo "- KSU_VER_CODE: $KSU_VER_CODE" >> $OUTPUT_LOG
+  echo "- APATCH: $APATCH" >> $OUTPUT_LOG
+  echo "- APATCH_VER_CODE: $APATCH_VER_CODE" >> $OUTPUT_LOG
+  echo "- MAGISK_VER_CODE: $MAGISK_VER_CODE" >> $OUTPUT_LOG
+  echo "- MAGISK_VER: $MAGISK_VER" >> $OUTPUT_LOG
 }
 
 show_system_info(){
-  ui_print "- Device brand: `getprop ro.product.brand`"
-  ui_print "- Device model: `getprop ro.product.model`"
-  ui_print "- Device codeName: `getprop ro.product.device`"
-  ui_print "- Device Architecture: $ARCH"
-  ui_print "- Android version: `getprop ro.build.version.release` API $API"
-  ui_print "- RAM space: `free -m|grep "Mem"|awk '{print $2}'`MB  used:`free -m|grep "Mem"|awk '{print $3}'`MB  free:$((`free -m|grep "Mem"|awk '{print $2}'`-`free -m|grep "Mem"|awk '{print $3}'`))MB"
-  ui_print "- SWAP space: `free -m|grep "Swap"|awk '{print $2}'`MB  used:`free -m|grep "Swap"|awk '{print $3}'`MB  free:`free -m|grep "Swap"|awk '{print $4}'`MB"
+  echo "- Device: $(getprop ro.product.brand) $(getprop ro.product.model) ($(getprop ro.product.device))"
+  echo "- Android $(getprop ro.build.version.release) (API $API), $ARCH"
+  mem_info=$(free -m)
+  ram_total=$(echo "$mem_info" | awk '/Mem/ {print $2}')
+  ram_used=$(echo "$mem_info" | awk '/Mem/ {print $3}')
+  ram_free=$((ram_total - ram_used))
+  swap_total=$(echo "$mem_info" | awk '/Swap/ {print $2}')
+  swap_used=$(echo "$mem_info" | awk '/Swap/ {print $3}')
+  swap_free=$(echo "$mem_info" | awk '/Swap/ {print $4}')
+  echo "- RAM Space: ${ram_total}MB  Used:${ram_used}MB  Free:${ram_free}MB"
+  echo "- SWAP Space: ${swap_total}MB  Used:${swap_used}MB  Free:${swap_free}MB"
+}
+
+abort_verify() {
+  echo "! $1"
+  echo "! This zip may be corrupted or have been maliciously modified!"
+  abort "! Please try to download again or get it from official source!"
 }
 
 extract() {
@@ -70,19 +97,34 @@ extract() {
   [ $junk_paths = true ] && opts="-oj"
 
   file_path=""
+  hash_path=""
   if [ $junk_paths = true ]; then
     file_path="$dir/$(basename "$file")"
+    hash_path="$VERIFY_DIR/$(basename "$file").sha256"
   else
     file_path="$dir/$file"
+    hash_path="$VERIFY_DIR/$file.sha256"
   fi
 
   unzip $opts "$zip" "$file" -d "$dir" >&2
-  [ -f "$file_path" ] || abort "$file not exists"
-  ui_print "- Extract $file -> $file_path" >&1
-  
+  [ -f "$file_path" ] || abort_verify "$file does not exist!"
+  echo "- Extract $file -> $file_path" >&1
+
+  unzip $opts "$zip" "$file.sha256" -d "$VERIFY_DIR" >&2
+  [ -f "$hash_path" ] || abort_verify "$file.sha256 does not exist!"
+
+  expected_hash="$(cat "$hash_path")"
+  calculated_hash="$(sha256sum "$file_path" | cut -d ' ' -f1)"
+  #echo "- EH: $expected_hash"
+  #echo "- CH: $calculated_hash"
+  if [ "$expected_hash" == "$calculated_hash" ]; then
+    echo "- Verified $file" >&1
+  else
+    abort_verify "Failed to verify $file"
+  fi
 }
 
 set_module_files_perm(){
-  ui_print "- Setting permissions"
+  echo "- Setting permissions"
   set_perm_recursive "$MODPATH" 0 0 0755 0644
 }
