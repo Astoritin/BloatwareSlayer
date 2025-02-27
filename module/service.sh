b4aeb1dc87b7d8960442597ab7424ab605eb74a7
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-02-27 23:12:30 +0800

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -10,15 +10,15 @@ MOD_NAME="$(sed -n 's/^name=\(.*\)/\1/p' "${MODDIR}/module.prop")"
 MOD_AUTHOR="$(sed -n 's/^author=\(.*\)/\1/p' "${MODDIR}/module.prop")"
 MOD_VER="$(sed -n 's/^version=\(.*\)/\1/p' "${MODDIR}/module.prop") ($(sed -n 's/^versionCode=\(.*\)/\1/p' "${MODDIR}/module.prop"))"
 
-echo "- Magisk Module Info------------------------------------------" >> $BS_LOG_FILE
-echo "$MOD_NAME" >> $BS_LOG_FILE
-echo "By $MOD_AUTHOR" >> $BS_LOG_FILE
+echo "--Magisk Module Info----------------------------------------------------------------------------------" >> $BS_LOG_FILE
+echo "- $MOD_NAME" >> $BS_LOG_FILE
+echo "- By $MOD_AUTHOR" >> $BS_LOG_FILE
 echo "- Version: $MOD_VER" >> $BS_LOG_FILE
 echo "- Current time stamp: $(date +"%Y-%m-%d %H:%M:%S")" >> $BS_LOG_FILE
 echo "- Starting service.sh..." >> $BS_LOG_FILE
-echo "- env Info----------------------------------------------------" >> $BS_LOG_FILE
+echo "--env Info--------------------------------------------------------------------------------------------" >> $BS_LOG_FILE
 env | sed 's/^/- /' >> $BS_LOG_FILE
-echo "- start service-----------------------------------------------" >> $BS_LOG_FILE
+echo "--start service---------------------------------------------------------------------------------------" >> $BS_LOG_FILE
 
 if [ -f "${MODDIR}/disable" ]; then
     echo '- Detect file "disable", module is already disabled' >> $BS_LOG_FILE
@@ -48,9 +48,9 @@ fi
         echo "- But system boot completely, deleting it..." >> $BS_LOG_FILE
         rm -rf "$BRICKED_STATUS"
         if [ $? -eq 0 ]; then
-            echo "- Bricked status clear" >> $BS_LOG_FILE
+            echo "- Bricked status cleared" >> $BS_LOG_FILE
         else
-            echo "- Failed to clear bricked status" >> $BS_LOG_FILE
+            echo "! Failed to clear bricked status" >> $BS_LOG_FILE
         fi
     else
         echo "- Bricked status file does not exist, nothing to delete" >> $BS_LOG_FILE
@@ -58,7 +58,7 @@ fi
 
     echo "- Boot complete! Final countdown: $TIMEOUT s" >> $BS_LOG_FILE
     echo "- service.sh case closed!" >> $BS_LOG_FILE
-    echo "--------------------------------------------------------------" >> $BS_LOG_FILE
+    echo "------------------------------------------------------------------------------------------------------" >> $BS_LOG_FILE
 
     MOD_DESC_OLD=$(sed -n 's/^description=//p' "$STATUS_DIR")
     ROOT_IMP=$(sed -n 's/^root=//p' "$STATUS_DIR")
