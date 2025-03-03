6182e0adff1c76b8a0b45937844c2f03742bfc0c
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-03-03 10:42:28 +0800

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -3,12 +3,20 @@ SKIPUNZIP=1
 
 CONFIG_DIR=/data/adb/bloatwareslayer
 LOG_DIR="$CONFIG_DIR/logs"
-MOD_NAME="$(grep_prop name "${TMPDIR}/module.prop")"
-MOD_VER="$(grep_prop version "${TMPDIR}/module.prop") ($(grep_prop versionCode "${TMPDIR}/module.prop"))"
 VERIFY_DIR="$TMPDIR/.aa_bs_verify"
 
+MOD_NAME="$(grep_prop name "${TMPDIR}/module.prop")"
+MOD_VER="$(grep_prop version "${TMPDIR}/module.prop") ($(grep_prop versionCode "${TMPDIR}/module.prop"))"
+
+
+if [ ! -d "$LOG_DIR" ]; then
+  echo "- $LOG_DIR does not exist"
+  mkdir -p "$LOG_DIR" || abort "! Failed to create $LOG_DIR!"
+  echo "- Created $LOG_DIR"
+fi
+
 echo "- Extract aautilities.sh"
-echo "- MODPATH: $MODPATH"
+# echo "- MODPATH: $MODPATH"
 unzip -o "$ZIPFILE" 'aautilities.sh' -d "$TMPDIR" >&2
 if [ ! -f "$TMPDIR/aautilities.sh" ]; then
   echo "! Failed to extract aautilities.sh!"
@@ -17,27 +25,21 @@ fi
 . "$TMPDIR/aautilities.sh"
 
 module_install_proc(){
-  echo "- Installing $MOD_NAME"
+  echo "- Setting up $MOD_NAME"
   echo "- Version: $MOD_VER"
   echo "- Extract module file(s)"
   extract "$ZIPFILE" 'aautilities.sh' "$VERIFY_DIR"
   extract "$ZIPFILE" 'customize.sh' "$VERIFY_DIR"
   extract "$ZIPFILE" 'aautilities.sh' "$MODPATH"
   extract "$ZIPFILE" 'module.prop' "$MODPATH"
-  extract "$ZIPFILE" 'post-fs-data.sh' "$MODPATH"
   extract "$ZIPFILE" 'service.sh' "$MODPATH"
   extract "$ZIPFILE" 'uninstall.sh' "$MODPATH"
-  if [ ! -d "$LOG_DIR" ]; then
-    echo "- $LOG_DIR does not exist"
-    mkdir -p "$LOG_DIR" || abort "! Failed to create $LOG_DIR!"
-    echo "- Create $LOG_DIR"
-  fi
   if [ ! -f "$CONFIG_DIR/target.txt" ]; then
     echo "- target.txt does not exist"
     extract "$ZIPFILE" 'target.txt' "$TMPDIR"
     mv "$TMPDIR/target.txt" "$CONFIG_DIR/target.txt" || abort "! Failed to create target.txt!"
   else
-    echo "- Detect existed target.txt"
+    echo "- Detect target.txt already existed"
     echo "- Skip overwriting target.txt"
   fi
 }
@@ -46,6 +48,5 @@ show_system_info
 install_env_check "$CONFIG_DIR"
 clean_old_logs "$LOG_DIR" 30
 module_install_proc
-debug_print_values "$LOG_DIR/log_install_$(date +"%Y-%m-%d_%H-%M-%S").txt"
 set_module_files_perm
 echo "- Welcome to use ${MOD_NAME}!"
