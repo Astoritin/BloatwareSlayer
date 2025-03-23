d4c173907ddb457dc0ccc64a19dbc085f8af1ca2
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-03-23 23:54:20 +0800

    Update post-fs-data.sh

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -9,16 +9,16 @@ EMPTY_DIR="$CONFIG_DIR/empty"
 TARGET_LIST="$CONFIG_DIR/target.conf"
 TARGET_LIST_BSA="$CONFIG_DIR/logs/target_bsa.conf"
 LOG_DIR="$CONFIG_DIR/logs"
-LOG_FILE="$LOG_DIR/bs_log_core_pfd_$(date +"%Y-%m-%d_%H-%M-%S").log"
+LOG_FILE="$LOG_DIR/bs_log_core_$(date +"%Y-%m-%d_%H-%M-%S").log"
 
 MODULE_PROP="$MODDIR/module.prop"
 MOD_NAME="$(sed -n 's/^name=\(.*\)/\1/p' "$MODULE_PROP")"
 MOD_AUTHOR="$(sed -n 's/^author=\(.*\)/\1/p' "$MODULE_PROP")"
 MOD_VER="$(sed -n 's/^version=\(.*\)/\1/p' "$MODULE_PROP") ($(sed -n 's/^versionCode=\(.*\)/\1/p' "$MODULE_PROP"))"
 
-SLAY_MODE="auto"
 UPDATE_TARGET_LIST=true
 AUTO_UPDATE_TARGET_LIST=true
+DISABLE_MODULE_AS_BRICK=true
 
 SYSTEM_APP_PATHS="/system/app /system/product/app /system/product/priv-app /system/priv-app /system/system_ext/app /system/system_ext/priv-app /system/vendor/app /system/vendor/priv-app"
 
@@ -30,14 +30,27 @@ brick_rescue() {
     #
     # BRICKED_STATUS: a empty file with a filename "bricked" located in /data/adb/bloatwareslayer
     # if detecting /data/adb/bloatwareslayer/bricked, module will skip mounting to prevent from being bricked by Bloatware Slayer itself
-    
+
+    logowl "Checking brick status"
+
     if [ -f "$BRICKED_STATUS" ]; then
         logowl "Detect flag bricked!" "FATAL"
-        logowl "Skip post-fs-data.sh process"
-        DESCRIPTION="[❌Disabled. Auto disabled from brick! Root: $ROOT_SOL] A Magisk module to remove bloatware in systemlessly way 🎉✨"
-        update_module_description "$DESCRIPTION" "$MODULE_PROP"
-        logowl "Skip mounting"
-        exit 1
+        if [ "$DISABLE_MODULE_AS_BRICK" = "true" ] && [ ! -f "$MODDIR/disable" ]; then
+            logowl "Detect flag DISABLE_MODULE_AS_BRICK=true"
+            logowl "But module itself has NOT been disabled"
+            logowl "Maybe $MOD_NAME is enabled by user manually"
+            logowl "Reset brick status"
+            rm -f "$BRICKED_STATUS"
+            logowl "$MOD_NAME will keep going"
+            return 0
+        else
+            logowl "Starting brick rescue"
+            logowl "Skip post-fs-data.sh process"
+            DESCRIPTION="[❌ Disabled. Auto disable from brick! Root: $ROOT_SOL] A Magisk module to remove bloatware in systemlessly way✨"
+            update_module_description "$DESCRIPTION" "$MODULE_PROP"
+            logowl "Skip mounting"
+            exit 1
+        fi
     else
         logowl "Flag bricked does NOT detect"
         logowl "$MOD_NAME will keep going"
@@ -53,11 +66,11 @@ config_loader() {
 
     auto_update_target_list=$(init_variables "auto_update_target_list" "$CONFIG_FILE")
     system_app_paths=$(init_variables "system_app_paths" "$CONFIG_FILE")
-    slay_mode=$(init_variables "slay_mode" "$CONFIG_FILE")
+    disable_module_as_brick=$(init_variables "disable_module_as_brick" "$CONFIG_FILE")
 
     verify_variables "auto_update_target_list" "$auto_update_target_list" "^(true|false)$"
     verify_variables "system_app_paths" "$system_app_paths" "^/system/[^/]+(/[^/]+)*$"
-    verify_variables "slay_mode" "$slay_mode" "^(auto|empty|slay)$"
+    verify_variables "disable_module_as_brick" "$disable_module_as_brick" "^(true|false)$"
 
 }
 
@@ -74,21 +87,20 @@ preparation() {
     #
     # $UPDATE_TARGET_LIST: different from $AUTO_UPDATE_TARGET_LIST, this boolean variable is for Bloatware Slayer itself (inner behavior) to judge whether need to update or not by checking the hashcode of $TARGET_LIST and $TARGET_LIST_BSA
 
-    logowl "Start preparing something"
-    touch -r "/system" "$LOG_DIR/timestamp_ref"
+    logowl "Some preparations"
 
-    # for mknod
-    cleanup_and_create "$MODDIR/system"
-    if [ -n "$MODDIR" ] && [ -d "$MODDIR/system" ]; then
-        cleanup_and_create "$MODDIR/system" 755
-    fi
     if [ -n "$MODDIR" ] && [ -d "$EMPTY_DIR" ]; then
-        cleanup_and_create "$EMPTY_DIR" 755
+        logowl "Remove old empty folder"
+        rm -rf "$EMPTY_DIR"
     fi
+    logowl "Create $EMPTY_DIR"
+    mkdir -p "$EMPTY_DIR"
+    logowl "Set permissions"
+    chmod 0755 "$EMPTY_DIR"
 
     if [ ! -f "$TARGET_LIST" ]; then
         logowl "Target list does NOT exist!" "FATAL"
-        DESCRIPTION="[❌Disabled. Target list does NOT exist! Root: $ROOT_SOL] A Magisk module to remove bloatware in systemlessly way🎉✨"
+        DESCRIPTION="[❌ No effect. Target list does NOT exist! Root: $ROOT_SOL] A Magisk module to remove bloatware in systemlessly way✨"
         update_module_description "$DESCRIPTION" "$MODULE_PROP"
         return 1
     fi
@@ -116,50 +128,25 @@ preparation() {
 
 }
 
-reset_timestamp() {
-    rts_path="${1:-$MODDIR/system}"
-    rts_timestamp="${2:-200901010000.00}"
-
-    if [ -f "$timestamp_ref" ]; then
-        find "$rts_path" -exec touch -r "$rts_timestamp" {} \;
-    elif echo "$2" | grep -qE '^[0-9]+([.][0-9]+)?$'; then
-        find "$rts_path" -exec touch -t "$rts_timestamp" {} \;
-    else
-        logowl "Failed: timestamp is not a valid value!" "ERROR"
-    fi
-}
-
 bloatware_slayer() {
     # bloatware_slayer: the core function for bloatware slayer
 
-    # check to use mount -o bind or mknod
-    is_mknod="false"
-
-    case "$SLAY_MODE" in
-        "auto")
-            if [ "$MAGISK_V_VER_CODE" -ge 28102 ] || [ "$KSU" ] || [ "$APatch" ]; then
-                is_mknod="true"
-            fi
-            ;;
-        "empty")
-            is_mknod="false"
-            ;;
-        "slay")
-            is_mknod="true"
-            ;;
-    esac
+    logowl "Slaying bloatwares"
 
     TOTAL_APPS_COUNT=0
     BLOCKED_APPS_COUNT=0
-    logowl "Start $MOD_NAME process"
+
     while IFS= read -r line; do
+
         if check_value_safety "target.conf" "$line"; then
             logowl "Current line: $line"
         else
             continue
         fi
+
         line=$(echo "$line" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
         first_char=$(printf '%s' "$line" | cut -c1)
+
         if [ -z "$line" ]; then
             logowl "Detect empty line, skip processing" "TIPS"
             continue
@@ -183,6 +170,7 @@ bloatware_slayer() {
                 ;;
         esac
         logowl "After processed: $package"
+
         TOTAL_APPS_COUNT=$((TOTAL_APPS_COUNT+1))
         for path in $SYSTEM_APP_PATHS; do
             first_char=$(printf '%s' "$line" | cut -c1)
@@ -200,47 +188,21 @@ bloatware_slayer() {
             else
                 app_path="$path/$package"
             fi
+
             logowl "Checking dir: $app_path"
             if [ -d "$app_path" ]; then
-                if [ "$is_mknod" = "true" ]; then
-                    parent_dir=$(dirname "$app_path")
-                    module_parent_dir="$MODDIR/$parent_dir"
-                    logowl "Create parent path: $module_parent_dir"
-                    mkdir -p "$module_parent_dir"
-
-                    logowl "Execute mknod $app_path c 0 0"
-                    mknod "$MODDIR/$app_path" c 0 0
-                    result_mknod="$?"
-                    logowl "Result: mknod (code: $result_chcon_p)"
-
-                    # logowl "Fix SELinux context"
-                    # chcon -R u:object_r:system_file:s0 "$parent_dir" 2>/dev/null
-                    # result_chcon_p="$?"
-                    # chcon -v u:object_r:system_file:s0 "$MODDIR/$app_path" 2>/dev/null
-                    # result_chcon_a="$?"
-                    # logowl "Result: chcon parent dir (code: $result_chcon_p), chcon app path (code: $result_chcon_a)"
-                    
-                    if [ $result_mknod -eq 0 ]; then
-                        logowl "Succeeded"
-                        BLOCKED_APPS_COUNT=$((BLOCKED_APPS_COUNT + 1))
-                        if [ "$UPDATE_TARGET_LIST" = true ] && [ "$AUTO_UPDATE_TARGET_LIST" = "true" ]; then
-                            echo "$app_path" >> "$TARGET_LIST_BSA"
-                        fi
-                        break
+                logowl "Execute mount -o bind $EMPTY_DIR $app_path"
+                mount -o bind "$EMPTY_DIR" "$app_path"
+                result_mount_bind=$?
+                if [ $result_mount_bind -eq 0 ]; then
+                    logowl "Succeeded (code: $result_mount_bind)"
+                    BLOCKED_APPS_COUNT=$((BLOCKED_APPS_COUNT + 1))
+                    if [ "$UPDATE_TARGET_LIST" = true ] && [ "$AUTO_UPDATE_TARGET_LIST" = "true" ]; then
+                        echo "$app_path" >> "$TARGET_LIST_BSA"
                     fi
+                    break
                 else
-                    logowl "Execute mount -o bind $EMPTY_DIR $app_path"
-                    mount -o bind "$EMPTY_DIR" "$app_path"
-                    if [ $? -eq 0 ]; then
-                        logowl "Succeeded"
-                        BLOCKED_APPS_COUNT=$((BLOCKED_APPS_COUNT + 1))
-                        if [ "$UPDATE_TARGET_LIST" = true ] && [ "$AUTO_UPDATE_TARGET_LIST" = "true" ]; then
-                            echo "$app_path" >> "$TARGET_LIST_BSA"
-                        fi
-                        break
-                    else
-                        logowl "Failed to mount: $app_path, error code: $?" "ERROR"
-                    fi
+                    logowl "Failed to mount: $app_path (code: $result_mount_bind)" "ERROR"
                 fi
             else
                 if [ "$first_char" = "/" ]; then
@@ -254,14 +216,11 @@ bloatware_slayer() {
     done < "$TARGET_LIST"
 
     if [ "$UPDATE_TARGET_LIST" = "true" ] && [ "$AUTO_UPDATE_TARGET_LIST" = "true" ]; then
-        logowl "Update target list" "TIPS"
+        logowl "Updating target list"
         cp -p "$TARGET_LIST_BSA" "$TARGET_LIST"
         chmod 0644 "$TARGET_LIST_BSA"
         chmod 0644 "$TARGET_LIST"
     fi
-
-    set_perm_recursive "$MODDIR" 0 0 0755 0644
-    reset_timestamp "$MODDIR/system" "$LOG_DIR/timestamp_ref"
 }
 
 module_status_update() {
@@ -270,6 +229,8 @@ module_status_update() {
     # BLOCKED_APPS_COUNT: the count of the APPs being blocked by Bloatware Slayer successfully
     # APP_NOT_FOUND: the count of the APPs not found or failed to block
 
+    logowl "Updating module status"
+
     APP_NOT_FOUND=$((TOTAL_APPS_COUNT - BLOCKED_APPS_COUNT))
     logowl "$TOTAL_APPS_COUNT APP(s) in total"
     logowl "$BLOCKED_APPS_COUNT APP(s) slain"
@@ -277,16 +238,16 @@ module_status_update() {
 
     if [ -f "$MODULE_PROP" ]; then
         if [ $BLOCKED_APPS_COUNT -gt 0 ]; then
-            DESCRIPTION="[😋Enabled. $BLOCKED_APPS_COUNT APP(s) slain, $APP_NOT_FOUND APP(s) missing, $TOTAL_APPS_COUNT APP(s) targeted in total, Root: $ROOT_SOL] 勝った、勝った、また勝ったぁーっと！！🎉✨"
+            DESCRIPTION="[😋 Enabled. $BLOCKED_APPS_COUNT APP(s) slain, $APP_NOT_FOUND APP(s) missing, $TOTAL_APPS_COUNT APP(s) targeted in total, Root: $ROOT_SOL] 勝った、勝った、また勝ったぁーっと！！🎉✨"
             if [ $APP_NOT_FOUND -eq 0 ]; then
-            DESCRIPTION="[😋Enabled. $BLOCKED_APPS_COUNT APP(s) slain. All targets neutralized! Root: $ROOT_SOL] 勝った、勝った、また勝ったぁーっと！！🎉✨"
+            DESCRIPTION="[😋 Enabled. $BLOCKED_APPS_COUNT APP(s) slain. All targets neutralized! Root: $ROOT_SOL] 勝った、勝った、また勝ったぁーっと！！🎉✨"
             fi
         else
             if [ $TOTAL_APPS_COUNT -gt 0 ]; then
-                DESCRIPTION="[😋Enabled. No APP slain yet, $TOTAL_APPS_COUNT APP(s) targeted in total, Root: $ROOT_SOL] 勝った、勝った、また勝ったぁーっと！！🎉✨"
+                DESCRIPTION="[😋 No effect. No APP slain yet, $TOTAL_APPS_COUNT APP(s) targeted in total, Root: $ROOT_SOL] 勝った、勝った、また勝ったぁーっと！！🎉✨"
             else
                 logowl "! Current blocked apps count: $TOTAL_APPS_COUNT <= 0"
-                DESCRIPTION="[❌Disabled. Abnormal status! Root: $ROOT_SOL] A Magisk module to remove bloatware in systemlessly way🎉✨"
+                DESCRIPTION="[❌ No effect. Abnormal status! Root: $ROOT_SOL] A Magisk module to remove bloatware in systemlessly way✨"
             fi
         fi
         update_module_description "$DESCRIPTION" "$MODULE_PROP"
@@ -311,5 +272,4 @@ bloatware_slayer
 module_status_update
 logowl "Variables before case closed"
 debug_print_values >> "$LOG_FILE"
-print_line
 logowl "post-fs-data.sh case closed!"
