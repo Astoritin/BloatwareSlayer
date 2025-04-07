735fd0fa14dd15946a1428ef38af50a93d49f086
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-04-07 11:33:35 +0800

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -3,6 +3,6 @@ MODDIR=${0%/*}
 
 CONFIG_DIR="/data/adb/bloatwareslayer"
 
-if [ -n "$CONFIG_DIR" ]; then
+if [ -n "$CONFIG_DIR" ] && [ -d "$CONFIG_DIR" ] && [ "$CONFIG_DIR" != "/" ]; then
     rm -rf "$CONFIG_DIR"
 fi
