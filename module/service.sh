3457281c0f00f3da691bcc143eb792780846f62c
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-05-07 23:29:47 +0800

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -1,195 +1,217 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
-CONFIG_DIR="/data/adb/bloatwareslayer"
-CONFIG_FILE="$CONFIG_DIR/settings.conf"
-BRICKED_STATUS="$CONFIG_DIR/bricked"
-LOG_DIR="$CONFIG_DIR/logs"
-LOG_FILE="$LOG_DIR/bs_brickd_$(date +"%Y%m%dT%H%M%S").log"
-TARGET_LIST_BSA="$LOG_DIR/target_bsa.conf"
+CONFIG_DIR="/data/adb/vbmetadisguiser"
+DEBUG=false
 
-MODULE_PROP="$MODDIR/module.prop"
+CONFIG_FILE="$CONFIG_DIR/vbmeta.conf"
+LOG_DIR="$CONFIG_DIR/logs"
+LOG_FILE="$LOG_DIR/vd_vbmetad_$(date +"%Y-%m-%d_%H-%M-%S").log"
+
+MODULE_PROP="${MODDIR}/module.prop"
 MOD_NAME="$(sed -n 's/^name=\(.*\)/\1/p' "$MODULE_PROP")"
 MOD_AUTHOR="$(sed -n 's/^author=\(.*\)/\1/p' "$MODULE_PROP")"
 MOD_VER="$(sed -n 's/^version=\(.*\)/\1/p' "$MODULE_PROP") ($(sed -n 's/^versionCode=\(.*\)/\1/p' "$MODULE_PROP"))"
-MOD_DESC_OLD="$(sed -n 's/^description=\(.*\)/\1/p' "$MODULE_PROP")"
 MOD_ROOT_DIR=$(dirname "$MODDIR")
-MOD_ZYGISKSU_PATH="${MOD_ROOT_DIR}/zygisksu"
 
-DEBUG=false
-BRICK_RESCUE=true
-SLAY_MODE=MB
-MB_UMOUNT_BIND=true
-BRICK_TIMEOUT=180
-DISABLE_MODULE_AS_BRICK=true
-UPDATE_DESC_ON_ACTION=false
+TRICKY_STORE_CONFIG_FILE="/data/adb/tricky_store/security_patch.txt"
+SHAMIKO_DIR="${MOD_ROOT_DIR}/zygisk_shamiko"
+SENSITIVE_PROPS_DIR="${MOD_ROOT_DIR}/sensitive_props"
+
+AVB_VERSION="2.0"
+VBMETA_SIZE="8192"
+BOOT_HASH="00000000000000000000000000000000"
+
+debug_props_info() {
+
+    print_line
+    debug_get_prop "ro.boot.vbmeta.device_state"
+    debug_get_prop "ro.boot.vbmeta.avb_version"
+    debug_get_prop "ro.boot.vbmeta.hash_alg"
+    debug_get_prop "ro.boot.vbmeta.size"
+    debug_get_prop "ro.boot.vbmeta.digest"
+    debug_get_prop "ro.crypto.state"
+    debug_get_prop "ro.build.version.security_patch"
+    debug_get_prop "ro.system.build.security_patch"
+    debug_get_prop "ro.vendor.build.security_patch"
+    print_line
+
+}
+
+debug_contains_and_reset_prop() {
+
+    prop_name="$1"
+    prop_contains_keyword="$2"
+    prop_new_value="$3"
+
+    if [ -z "$prop_name" ] || [ -z "$prop_contains_keyword" ] || [ -z "$prop_new_value" ]; then
+        logowl "Required parameters is missing!" "ERROR"
+        return 1
+    fi
+
+    prop_current_value=$(resetprop "$prop_name")
+
+    if echo "$prop_current_value" | grep -q "$prop_contains_keyword"; then
+        resetprop "$prop_name" "$prop_new_value"
+        return 0
+    fi
+
+}
 
 config_loader() {
 
     logowl "Load config"
-
+    
     debug=$(init_variables "debug" "$CONFIG_FILE")
-    brick_rescue=$(init_variables "brick_rescue" "$CONFIG_FILE")
-    brick_timeout=$(init_variables "brick_timeout" "$CONFIG_FILE")
-    disable_module_as_brick=$(init_variables "disable_module_as_brick" "$CONFIG_FILE")
-    slay_mode=$(init_variables "slay_mode" "$CONFIG_FILE")
-    mb_umount_bind=$(init_variables "mb_umount_bind" "$CONFIG_FILE")
-    update_desc_on_action=$(init_variables "update_desc_on_action" "$CONFIG_FILE")
+    avb_version=$(init_variables "avb_version" "$CONFIG_FILE")
+    vbmeta_size=$(init_variables "vbmeta_size" "$CONFIG_FILE")
+    boot_hash=$(init_variables "boot_hash" "$CONFIG_FILE")
+    crypto_state=$(init_variables "crypto_state" "$CONFIG_FILE")
 
     verify_variables "debug" "$debug" "^(true|false)$"
-    verify_variables "brick_rescue" "$brick_rescue" "^(true|false)$"
-    verify_variables "brick_timeout" "$brick_timeout" "^[1-9][0-9]*$"
-    verify_variables "disable_module_as_brick" "$disable_module_as_brick" "^(true|false)$"
-    verify_variables "slay_mode" "$slay_mode" "^(MB|MN|MR)$"
-    verify_variables "mb_umount_bind" "$mb_umount_bind" "^(true|false)$"
-    verify_variables "update_desc_on_action" "$update_desc_on_action" "^(true|false)$"
+    verify_variables "avb_version" "$avb_version" "^[1-9][0-9]*\.[0-9]*$|^[1-9][0-9]*$"
+    verify_variables "vbmeta_size" "$vbmeta_size" "^[1-9][0-9]*$"
+    verify_variables "boot_hash" "$boot_hash" "^[0-9a-fA-F]{64}$"
+    verify_variables "crypto_state" "$crypto_state" "^encrypted$|^unencrypted$|^unsupported$"
 
 }
+
+vbmeta_disguiser() {
+
+    logowl "Disguise VBMeta partition status"
+
+    resetprop -n "ro.boot.vbmeta.device_state" "locked"
+    resetprop -n "ro.boot.vbmeta.hash_alg" "sha256"
+
+    if [ -s "$CONFIG_FILE" ]; then
+        resetprop -n "ro.boot.vbmeta.digest" "$BOOT_HASH"
+        resetprop -n "ro.boot.vbmeta.size" "$VBMETA_SIZE"
+        resetprop -n "ro.boot.vbmeta.avb_version" "$AVB_VERSION"
+    fi
+
+}
+
+encryption_disguiser(){
+
+    logowl "Disguise Data partition encryption state"
+
+    [ -n "$CRYPTO_STATE" ] && resetprop -n "ro.crypto.state" "$CRYPTO_STATE"
+
+}
+
+module_status_update() {
+
+    desc_max=3
+
+    logowl "Update module status"
+
+    update_count=0    
+    vbmeta_version=$(getprop 'ro.boot.vbmeta.avb_version')
+    vbmeta_digest=$(getprop 'ro.boot.vbmeta.digest' | cut -c1-12 | tr '[:lower:]' '[:upper:]')
+    ellipsis="(...)"
+    vbmeta_digest="${vbmeta_digest}${ellipsis}"
+    vbmeta_hash_alg=$(getprop 'ro.boot.vbmeta.hash_alg' | tr '[:lower:]' '[:upper:]')
+    vbmeta_size=$(getprop 'ro.boot.vbmeta.size')
+    device_state=$(getprop 'ro.boot.vbmeta.device_state')
+    crypto_state=$(getprop 'ro.crypto.state')
+    security_patch=$(getprop 'ro.build.version.security_patch')
+
+    if [ -z "$vbmeta_digest" ] || echo "$vbmeta_digest" | grep -qE '^0+$'; then
+        desc_vbmeta="❓VBMeta Hash: N/A"
+    else
+        desc_vbmeta="VBMeta Hash: $vbmeta_digest ($vbmeta_hash_alg)"
+        update_count=$((update_count + 1))
+    fi
+    
+    desc_avb="AVB ${vbmeta_version:-N/A} (${device_state:-N/A})"
+
+    [ "$crypto_state" = "encrypted" ] && desc_crypto="🔒"
+    [ "$crypto_state" = "unencrypted" ] && desc_crypto="🔓"
+    [ "$crypto_state" = "unsupported" ] && desc_crypto="❌"
+    [ -n "$desc_crypto" ] && update_count=$((update_count + 1))
+    desc_crypto="${desc_crypto}Data partition: $crypto_state"
+
+    if [ ! -e "$TRICKY_STORE_CONFIG_FILE" ] && [ ! -e "$CONFIG_FILE" ]; then
+        desc_ts_sp="❓Security patch config does NOT exist"
+    elif [ ! -f "$TRICKY_STORE_CONFIG_FILE" ] && [ ! -f "$CONFIG_FILE" ]; then
+        desc_ts_sp="❌Security patch config abnormal"
+    elif [ ! -s "$TRICKY_STORE_CONFIG_FILE" ] && [ ! -s "$CONFIG_FILE" ]; then
+        desc_ts_sp="❓Security patch: N/A"
+    else
+        desc_ts_sp="Security patch: $security_patch"
+        update_count=$((update_count + 1))
+    fi
+
+    desc_active=""
+    if [ "$update_count" -eq "$desc_max" ]; then
+        desc_active="✅All Done."
+    elif [ "$update_count" -gt 0 ]; then
+        desc_active="✅Done."
+    else
+        desc_active="❌No effect."
+    fi
+    
+    [ -n "$desc_active" ] && DESCRIPTION="[$desc_active $desc_vbmeta, $desc_avb, $desc_ts_sp, $desc_crypto] A Magisk module to disguise the props of vbmeta, security patch date and encryption status."
+    [ -z "$desc_active" ] && DESCRIPTION="[$desc_vbmeta, $desc_avb, $desc_ts_sp, $desc_crypto] A Magisk module to disguise the props of vbmeta, security patch date and encryption status."
+    [ -n "$DESCRIPTION" ] && update_config_value "description" "$DESCRIPTION" "$MODULE_PROP" "true"
+
+} >> "$LOG_FILE" 2>&1
+
+soft_bootloader_spoof() {
+
+    if [ -e "$SHAMIKO_DIR" ] || [ -e "$SENSITIVE_PROPS_DIR" ]; then
+        return 0
+    fi
+
+    logowl "Reset specific properties"
+
+    resetprop -w sys.boot_completed 0
+
+    resetprop -n "ro.boot.vbmeta.device_state" "locked"
+    resetprop -n "ro.boot.verifiedbootstate" "green"
+    resetprop -n "ro.boot.flash.locked" "1"
+    resetprop -n "ro.boot.veritymode" "enforcing"
+    resetprop -n "ro.boot.warranty_bit" "0"
+    resetprop -n "ro.warranty_bit" "0"
+    resetprop -n "ro.debuggable" "0"
+    resetprop -n "ro.force.debuggable" "0"
+    resetprop -n "ro.secure" "1"
+    resetprop -n "ro.adb.secure" "1"
+    resetprop -n "ro.build.type" "user"
+    resetprop -n "ro.build.tags" "release-keys"
+    resetprop -n "ro.vendor.boot.warranty_bit" "0"
+    resetprop -n "ro.vendor.warranty_bit" "0"
+    resetprop -n "vendor.boot.vbmeta.device_state" "locked"
+    resetprop -n "vendor.boot.verifiedbootstate" "green"
+
+    resetprop -n "sys.oem_unlock_allowed" "0"
+    resetprop -n "ro.oem_unlock_supported" "0"
+
+    resetprop -n "ro.boot.realmebootstate" "green"
+    resetprop -n "ro.boot.realme.lockstate" "1"
+
+    resetprop -n "ro.secureboot.lockstate" "locked"
+
+    resetprop -n "init.svc.flash_recovery" "stopped"
+
+    debug_contains_and_reset_prop "ro.bootmode" "recovery" "unknown"
+    debug_contains_and_reset_prop "ro.boot.bootmode" "recovery" "unknown"
+    debug_contains_and_reset_prop "vendor.boot.bootmode" "recovery" "unknown"
+
+}
+
 
 . "$MODDIR/aautilities.sh"
 
 init_logowl "$LOG_DIR"
 module_intro >> "$LOG_FILE"
-show_system_info >> "$LOG_FILE"
+show_system_info
 print_line
 logowl "Start service.sh"
 config_loader
-print_line
-[ "$UPDATE_DESC_ON_ACTION" = true ] && denylist_enforcing_status_update
-
-{    
-
-    logowl "Current booting timeout: $BRICK_TIMEOUT"
-    while [ "$(getprop sys.boot_completed)" != "1" ]; do
-
-        if [ "$BRICK_RESCUE" = false ]; then
-            logowl "Detect flag BRICK_RESCUE=false" "WARN"
-            logowl "$MOD_NAME will NOT take action as brick occurred!" "WARN"
-            break
-        fi
-
-        if [ $BRICK_TIMEOUT -le "0" ]; then
-            print_line
-            logowl "Detect failed to boot after reaching the set limit!" "FATAL"
-            logowl "Your device may be bricked by $MOD_NAME!"
-            logowl "Mark status as bricked"
-            touch "$BRICKED_STATUS"
-            if [ "$DISABLE_MODULE_AS_BRICK" = true ]; then
-                logowl "Detect flag DISABLE_MODULE_AS_BRICK=true"
-                logowl "Disable $MOD_NAME"
-                touch "$MODDIR/disable"
-            else
-                logowl "Detect flag DISABLE_MODULE_AS_BRICK=false"
-            fi
-            DESCRIPTION="[❌No effect. Auto disable from brick! ✨Root: $ROOT_SOL_DETAIL] A Magisk module to remove bloatware in systemless way."
-            update_config_value "description" "$DESCRIPTION" "$MODULE_PROP"
-            logowl "Rebooting"
-            logowl "Execute: reboot -f"
-            reboot -f
-            sleep 5
-            logowl "Reboot command does NOT take effect, exiting"
-            exit 1
-        fi
-        BRICK_TIMEOUT=$((BRICK_TIMEOUT-1))
-        sleep 1
-    done
-
-    logowl "Congratulations! Boot complete!"
-    logowl "Current countdown: $BRICK_TIMEOUT s"
-    rm -f "$BRICKED_STATUS"
-    if [ $? -eq 0 ]; then
-        logowl "Bricked status reset"
-    else
-        logowl "Failed to reset bricked status" "FATAL"
-    fi
-    print_line
-
-    if [ "$SLAY_MODE" = "MB" ] && [ "$MB_UMOUNT_BIND" = true ]; then
-        logowl "$MOD_NAME is running on Mount Bind mode"
-        logowl "Detect flag MB_UMOUNT_BIND=true"
-        logowl "Execute umount process"
-        if [ ! -f "$TARGET_LIST_BSA" ]; then
-            logowl "Invalid Target List ($MOD_NAME arranged) file!" "ERROR"
-        else
-            lines_count=0
-
-            while IFS= read -r line || [ -n "$line" ]; do
-                lines_count=$((lines_count + 1))
-
-                if ! check_value_safety "line $lines_count" "$line"; then
-                    continue
-                fi
-
-                line=$(echo "$line" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-                first_char=$(printf '%s' "$line" | cut -c1)
-                if [ -z "$line" ]; then
-                    [ "$DEBUG" = true ] && logowl "Empty in line $lines_count (code: 2)" "WARN"
-                    continue
-                elif [ "$first_char" = "#" ]; then
-                    [ "$DEBUG" = true ] && logowl "Comment symbol in line $lines_count (code: 3)" "WARN"
-                    continue
-                fi
-
-                package=$(echo "$line" | cut -d '#' -f1)
-                package=$(echo "$package" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-                if [ -z "$package" ]; then
-                    [ "$DEBUG" = true ] && logowl "Only comment left in line $lines_count, skip processing" "TIPS"
-                    continue
-                fi
-                case "$package" in
-                    *\\*)
-                        [ "$DEBUG" = true ] && logowl "Replace '\\' with '/' in path: $package" "WARN"
-                        package=$(echo "$package" | sed -e 's/\\/\//g')
-                        ;;
-                esac
-                logowl "Process path: $package"
-                umount -f $package
-                result_umount=$?
-                logowl "Execute: umount -f $package"
-                app_name="$(basename "$package")"
-                if [ $result_umount -eq 0 ]; then
-                    logowl "Mount point $app_name has been unmounted" "TIPS"
-                else
-                    logowl "Failed to unmount point $app_name (code: $result_umount)" "WARN"
-                fi
-
-            done < "$TARGET_LIST_BSA"
-        fi
-    fi
-    debug_print_values >> "$LOG_FILE"
-    print_line
-    logowl "service.sh case closed!"
-    
-    MOD_REAL_TIME_DESC=""
-    while true; do
-        if [ "$UPDATE_DESC_ON_ACTION" = false ]; then
-            logowl "Detect flag UPDATE_DESC_ON_ACTION=false"
-            logowl "Exit background task"
-            exit 0
-        fi
-        if [ -f "$MODDIR/update" ]; then
-            MOD_CURRENT_STATUS="update"
-        elif [ -f "$MODDIR/remove" ]; then
-            MOD_CURRENT_STATUS="remove"
-        elif [ -f "$MODDIR/disable" ]; then
-            MOD_CURRENT_STATUS="disable"
-        else
-            MOD_CURRENT_STATUS="enable"
-        fi
-
-        if [ "$MOD_CURRENT_STATUS" = "update" ]; then
-            logowl "Detect update status"
-            logowl "Exit background task"
-            exit 0
-        elif [ "$MOD_CURRENT_STATUS" = "remove" ]; then
-            MOD_REAL_TIME_DESC="[🗑️Reboot to remove. ✨Root: $ROOT_SOL_DETAIL] A Magisk module to remove bloatware in systemless way."
-        elif [ "$MOD_CURRENT_STATUS" = "disable" ]; then
-            MOD_REAL_TIME_DESC="[❌OFF or reboot to turn off. ✨Root: $ROOT_SOL_DETAIL] A Magisk module to remove bloatware in systemless way."
-        elif [ "$MOD_CURRENT_STATUS" = "enable" ]; then
-            MOD_REAL_TIME_DESC="$MOD_DESC_OLD"
-        fi
-        denylist_enforcing_status_update "$MOD_REAL_TIME_DESC"
-        sleep 5
-    done
-
-} &
+vbmeta_disguiser
+encryption_disguiser
+module_status_update
+soft_bootloader_spoof
+debug_props_info
+logowl "service.sh case closed!"
