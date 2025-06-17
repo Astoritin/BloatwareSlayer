506785a081023710f72a83b89b0f023d50de27f5
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-06-17 23:56:43 +0800

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -32,7 +32,6 @@ logowl "Install from $ROOT_SOL app"
 logowl "Essential check"
 extract "$ZIPFILE" 'aa-util.sh' "$VERIFY_DIR"
 extract "$ZIPFILE" 'customize.sh' "$VERIFY_DIR"
-logowl_clean "$LOG_DIR" 20
 logowl "Extract module files"
 extract "$ZIPFILE" 'aa-util.sh' "$MODPATH"
 extract "$ZIPFILE" 'module.prop' "$MODPATH"
@@ -40,24 +39,19 @@ extract "$ZIPFILE" 'post-fs-data.sh' "$MODPATH"
 extract "$ZIPFILE" 'service.sh' "$MODPATH"
 extract "$ZIPFILE" 'action.sh' "$MODPATH"
 extract "$ZIPFILE" 'uninstall.sh' "$MODPATH"
-logowl "Extract default config"
-if [ ! -f "$CONFIG_DIR/target.conf" ]; then
-    logowl "target.conf does NOT exist"
-    extract "$ZIPFILE" 'target.conf' "$CONFIG_DIR"
-else
-    logowl "target.conf already exists"
-    logowl "target.conf will NOT be overwritten"
-fi
-if [ ! -f "$CONFIG_FILE" ]; then
-    logowl "settings.conf does NOT exist"
-    extract "$ZIPFILE" 'settings.conf' "$CONFIG_DIR"
-else
-    logowl "settings.conf already exists"
-    logowl "settings.conf will NOT be overwritten"
+if [ ! -f "$CONFIG_DIR/target.conf" ] || [ ! -f "$CONFIG_FILE" ]; then
+    if [ ! -f "$CONFIG_DIR/target.conf" ]; then
+        logowl "Extract target list"
+        extract "$ZIPFILE" 'target.conf' "$CONFIG_DIR"
+    fi
+    if [ ! -f "$CONFIG_FILE" ]; then
+        logowl "Extract config file"
+        extract "$ZIPFILE" 'settings.conf' "$CONFIG_DIR"
+    fi
 fi
 [ "$VERIFY_DIR" != "/" ] && rm -rf "$VERIFY_DIR"
 logowl "Set permission"
 set_permission_recursive "$MODPATH" 0 0 0755 0644
 logowl "Welcome to use $MOD_NAME!"
-DESCRIPTION="[✨Reboot to take effect. ⚙️Root: $ROOT_SOL_DETAIL] $MOD_INTRO"
+DESCRIPTION="[✨Please check items inside target.txt carefully before reboot. ⚙️Root: $ROOT_SOL_DETAIL] $MOD_INTRO"
 update_config_var "description" "$DESCRIPTION" "$MODPATH/module.prop"
