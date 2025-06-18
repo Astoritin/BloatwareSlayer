4029a490c95fbb86e22c4d1e08b14708da807ca0
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-06-18 23:52:29 +0800

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -4,6 +4,7 @@ SKIPUNZIP=1
 CONFIG_DIR="/data/adb/bloatwareslayer"
 
 CONFIG_FILE="$CONFIG_DIR/settings.conf"
+TARGET_LIST="$CONFIG_DIR/target.conf"
 LOG_DIR="$CONFIG_DIR/logs"
 
 VERIFY_DIR="$TMPDIR/.aa_verify"
@@ -14,7 +15,6 @@ MOD_INTRO="Remove bloatware in systemless way."
 
 [ ! -d "$VERIFY_DIR" ] && mkdir -p "$VERIFY_DIR"
 
-echo "- Extract aa-util.sh"
 unzip -o "$ZIPFILE" 'aa-util.sh' -d "$TMPDIR" >&2
 if [ ! -f "$TMPDIR/aa-util.sh" ]; then
     echo "! Failed to extract aa-util.sh!"
@@ -26,32 +26,25 @@ fi
 logowl "Setting up $MOD_NAME"
 logowl "Version: $MOD_VER"
 logowl_init "$LOG_DIR"
-install_env_check
 show_system_info
+install_env_check
 logowl "Install from $ROOT_SOL app"
-logowl "Essential check"
-extract "$ZIPFILE" 'aa-util.sh' "$VERIFY_DIR"
 extract "$ZIPFILE" 'customize.sh' "$VERIFY_DIR"
-logowl "Extract module files"
 extract "$ZIPFILE" 'aa-util.sh' "$MODPATH"
 extract "$ZIPFILE" 'module.prop' "$MODPATH"
 extract "$ZIPFILE" 'post-fs-data.sh' "$MODPATH"
 extract "$ZIPFILE" 'service.sh' "$MODPATH"
 extract "$ZIPFILE" 'action.sh' "$MODPATH"
 extract "$ZIPFILE" 'uninstall.sh' "$MODPATH"
-if [ ! -f "$CONFIG_DIR/target.conf" ] || [ ! -f "$CONFIG_FILE" ]; then
-    if [ ! -f "$CONFIG_DIR/target.conf" ]; then
-        logowl "Extract target list"
-        extract "$ZIPFILE" 'target.conf' "$CONFIG_DIR"
-    fi
-    if [ ! -f "$CONFIG_FILE" ]; then
-        logowl "Extract config file"
-        extract "$ZIPFILE" 'settings.conf' "$CONFIG_DIR"
-    fi
+if [ ! -f "$TARGET_LIST" ]; then
+    extract "$ZIPFILE" 'target.conf' "$CONFIG_DIR"
+fi
+if [ ! -f "$CONFIG_FILE" ]; then
+    extract "$ZIPFILE" 'settings.conf' "$CONFIG_DIR"
 fi
 [ "$VERIFY_DIR" != "/" ] && rm -rf "$VERIFY_DIR"
 logowl "Set permission"
 set_permission_recursive "$MODPATH" 0 0 0755 0644
 logowl "Welcome to use $MOD_NAME!"
-DESCRIPTION="[✨Please check items inside target.txt carefully before reboot. ⚙️Root: $ROOT_SOL_DETAIL] $MOD_INTRO"
+DESCRIPTION="[✨Reboot to take effect. Make sure you have checked the items in $TARGET_LIST carefully before reboot your device. ⚙️Root: $ROOT_SOL_DETAIL] $MOD_INTRO"
 update_config_var "description" "$DESCRIPTION" "$MODPATH/module.prop"
