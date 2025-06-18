ced2726b96e694d485cc82417382b21627bc44c1
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-06-19 00:06:48 +0800

    Update action.sh

--- module/action.sh
+++ module/action.sh
@@ -31,23 +31,25 @@ for fm in $ROOT_FILE_MANAGERS; do
     PKG=${fm%/*}
 
     if pm path "$PKG" >/dev/null 2>&1; then
-        echo "---------------------------------------------------"
-        echo "- Launching $PKG"
+        echo "> Launching $PKG"
         am start -n "$fm" "file://$CONFIG_DIR"
         result_action="$?"
         echo "- am start -n $fm file://$CONFIG_DIR ($result_action)"
         if [ $result_action -eq 0 ]; then
+            echo "---------------------------------------------------"
+            echo "- Case closed!"
+            sleep 2
             exit 0
         fi
     else
-        echo "- $PKG is NOT installed"
+        echo "? $PKG is NOT installed"
         sleep 1
     fi
 
 done
 
 echo "---------------------------------------------------"
-echo "- Not any available Root File Explorer found"
-echo "- Please open config dir manually if needed"
+echo "! Not any available Root File Explorer found!"
+echo "! Please open config dir manually if needed!"
 echo "---------------------------------------------------"
 echo "- Case closed!"
